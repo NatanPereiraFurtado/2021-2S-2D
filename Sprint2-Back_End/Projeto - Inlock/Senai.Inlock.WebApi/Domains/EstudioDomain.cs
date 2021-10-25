d6a9ded090757403b5e6e9a47037e7274ccd7491
NatanPereiraFurtado <85697428+NatanPereiraFurtado@users.noreply.github.com>
2021-10-25 01:56:04 -0300

    Add files via upload

---
+++ Sprint2-Back_End/Projeto - Inlock/Senai.Inlock.WebApi/Domains/EstudioDomain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprint2___Projeto_Inlock.Domains
+{
+    public class EstudioDomain
+    {
+        public int idEstudio { get; set; }
+        public string nomeEstudio { get; set; }
+    }
+}
---
+++ Sprint2-Back_End/Projeto - Inlock/Senai.Inlock.WebApi/Domains/JogosDomain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sprint2___Projeto_Inlock.Domains
+{
+    public class JogosDomain
+    {
+        public int idJogo { get; set; }
+        public string nomeJogo { get; set; }
+        public string descrição { get; set; }
+        public int idEstudio { get; set; }
+        public string nomeEstudio { get; set; }
+        public DateTime dataLançamento { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
